6ca4094599d7704ce2683c7df74ec7f7ab63d2f4
CurlyBraceTT <ucan.not.advance@gmail.com>
2018-07-23 19:18:38 +0300

    Week1 Complete
     + MaxPairwiseProduct

---
+++ CourseraAlgorithmicToolbox/Week1/MaxPairwiseProduct.cs
@@ -0,0 +1,54 @@
+﻿using System;
+
+namespace CourseraAlgorithmicToolbox.Week1
+{
+    // Repl https://repl.it/@CurlyBrace/MaxPairwiseProduct
+    public class MaxPairwiseProduct
+    {
+        public static void Main(string[] args)
+        {
+            var length = ulong.Parse(Console.ReadLine());
+            var numbers = new ulong[length];
+
+            var numbersStr = Console.ReadLine();
+            string[] tokens = numbersStr.Split(' ');
+
+            ulong[] convertedItems = Array.ConvertAll(tokens, ulong.Parse);
+
+            for (ulong i = 0; i < length; i++)
+            {
+                numbers[i] = convertedItems[i];
+            }
+
+            if (length < 2)
+            {
+                throw new ArgumentException("Should be at least one pair");
+            }
+
+            ulong max1 = numbers[0];
+            ulong max2 = numbers[1];
+
+            if (max2 > max1)
+            {
+                var temp = max1;
+                max1 = max2;
+                max2 = temp;
+            }
+
+            for (ulong i = 2; i < length; i++)
+            {
+                if (numbers[i] > max1)
+                {
+                    max2 = max1;
+                    max1 = numbers[i];
+                }
+                else if (numbers[i] > max2)
+                {
+                    max2 = numbers[i];
+                }
+            }
+
+            Console.WriteLine(max1 * max2);
+        }
+    }
+}
---
+++ CourseraAlgorithmicToolbox/Week1/Week1.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+using Xunit;
+
+namespace CourseraAlgorithmicToolbox.Week1
+{
+    public class Week1
+    {
+        [Theory]
+        [InlineData(6, "3", "1 2 3")]
+        [InlineData(9000000000, "2", "100000 90000")]
+        public void MaxPairwiseProductTest(ulong expected, params string[] args)
+        {
+            Utils.SetInput(args);
+            var output = Utils.SetOutput();
+            MaxPairwiseProduct.Main(args);
+            var result = ulong.Parse(output.ToString());
+            Assert.Equal(expected, result);
+        }
+    }
+}
