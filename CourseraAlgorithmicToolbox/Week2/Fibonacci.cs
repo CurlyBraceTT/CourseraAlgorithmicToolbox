2cfd02fe7c8d04b41897329fc47969bc82767846
CurlyBraceTT <ucan.not.advance@gmail.com>
2018-07-23 20:33:48 +0300

    Week2 Fibonacci
    + Fibonacci

---
+++ CourseraAlgorithmicToolbox/Week2/Fibonacci.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace CourseraAlgorithmicToolbox.Week2
+{
+    public class Fibonacci
+    {
+        private static ulong Calculate(ulong n)
+        {
+            if (n <= 1)
+            {
+                return n;
+            }
+
+            ulong f0 = 0;
+            ulong f1 = 1;
+            ulong f = 0;
+
+            for (ulong i = 2; i <= n; i++)
+            {
+                f = f0 + f1;
+
+                f0 = f1;
+                f1 = f;
+            }
+
+            return f;
+        }
+
+        public static void Main(string[] args)
+        {
+            var n = ulong.Parse(Console.ReadLine());
+            var result = Calculate(n);
+            Console.WriteLine(result);
+        }
+    }
+}
---
+++ CourseraAlgorithmicToolbox/Week2/Week2.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace CourseraAlgorithmicToolbox.Week2
+{
+    public class Week2
+    {
+        [Theory]
+        [InlineData(1, "1")]
+        [InlineData(55, "10")]
+        public void FibonacciFact(ulong expected, params string[] args)
+        {
+            using (ConsoleExaminator<ulong>.Exam(expected, args))
+            {
+                Fibonacci.Main(args);
+            }
+        }
+    }
+}
